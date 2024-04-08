58b49cf151e210cc589dd302ae21db97c1f84bb0
Luiz Felipe Florencio Monteiro <165718604+lzfelipe21@users.noreply.github.com>
2024-04-08 14:05:20 -0300

    Update exercicio06 AULA02.py

--- Aula 02/exercicio06 AULA02.py
+++ Aula 02/exercicio06 AULA02.py
@@ -1,11 +1,13 @@
-"""entrada01 = 3:45
-entrada02 = 14:20
-saida = 6:05"""
-
 hora1 = int(input("Primeira Hora:"))
 minuto1 = int(input("Primeiros Minutos:"))
 hora2 = int(input("Segundo Hora:"))
 minuto2 = int(input("Segundos Minutos:"))
+
+if hora1 >= 12:
+    hora1 -= 12
+
+if hora2 >= 12:
+    hora2 -=12
 
 horas = hora1 + hora2
 minutos = minuto1 + minuto2
@@ -13,11 +15,6 @@ minutos = minuto1 + minuto2
 if minutos >= 60:
     minutos -= 60
     horas += 1
-
-if horas > 12:
-    horas -= 12
-elif horas < 12:
-    horas +=12
 
 if minutos < 10:
     print(f"{int(horas)}:0{minutos}")
