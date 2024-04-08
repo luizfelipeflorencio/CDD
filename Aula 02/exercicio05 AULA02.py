9420d1ebcb0f16958f283bf2500fa2052d28fc9f
Luiz Felipe Florencio Monteiro <165718604+lzfelipe21@users.noreply.github.com>
2024-04-08 13:08:57 -0300

    Add files via upload

---
+++ Aula 02/exercicio05 AULA02.py
@@ -0,0 +1,8 @@
+num = int(input("digite o numero: "))
+
+if num==0:
+    print("Nulo")
+elif num%2 == 0:
+    print("Par")
+else:
+    print("Impar")
---
+++ Aula 02/exercicio06 AULA02.py
@@ -0,0 +1,4 @@
+"""entrada01 = 3:45
+entrada02 = 14:20
+saida = 6:05"""
+
