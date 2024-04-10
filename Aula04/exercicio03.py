40381eb2236f480fd4234b69183923cc1b85b488
Luiz Felipe Florencio Monteiro <165718604+lzfelipe21@users.noreply.github.com>
2024-04-10 15:36:30 -0300

    Add files via upload

---
+++ Aula04/exercicio03.py
@@ -0,0 +1,8 @@
+numero1 = int(input("Digite um numero: "))
+if numero1 != 0 or numero1 > 0:
+    for x in range(1, numero1+1):
+        print(x,end=" ")
+else: numero2 = int(input("Digite um numero: "))
+if numero2 != 0 or numero2 > 0:
+    for x in range(1, numero2+1):
+        print(x,end=" ")
