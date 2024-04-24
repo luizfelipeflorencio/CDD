c9da46d53482ec14e2c33714bef65c250fc8c99f
Luiz Felipe Florencio Monteiro <165718604+luizfelipeflorencio@users.noreply.github.com>
2024-04-24 15:48:25 -0300

    Update R7.py

--- Revisao/pythonProject3/R7.py
+++ Revisao/pythonProject3/R7.py
@@ -1,7 +1,14 @@
-vezes = "S"
-while vezes == "S" or vezes == "s":
-    base = float(input("Digite a base do triangulo: "))
-    altura = float(input("Digite a altura do triangulo: "))
-    a = (base*altura)/2
-    print("A area do triangulo é:",a)
-    vezes = input("Gostaria de fazer novamente (S/N): ")
+#triangulo
+sel = 0
+while sel != 3:
+    sel = int(input("Qual vc quer calcular? triangulo(1), quadrado(2) ou sair do sistema(3): "))
+    if sel == 1:
+            base = float(input("Digite a base do triangulo: "))
+            altura = float(input("Digite a altura do triangulo: "))
+            a = (base * altura)/2
+            print("A area do triangulo é:",a)
+    #quadrado
+    if sel == 2:
+            altura = float(input("Digite a altura do quadrado: "))
+            a = altura * 4
+            print("A area do quadrado é:",a)
