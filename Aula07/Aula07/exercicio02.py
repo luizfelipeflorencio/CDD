33c94c41f12eacddacf65a14b023fc357c83c54a
Luiz Felipe Florencio Monteiro <165718604+luizfelipeflorencio@users.noreply.github.com>
2024-05-13 17:05:51 -0300

    Update exercicio02.py

--- Aula07/Aula07/exercicio02.py
+++ Aula07/Aula07/exercicio02.py
@@ -9,3 +9,21 @@ valor = float(input("Digite o preço: "))
 retorno = estoque (produto,quant,valor)
 soma = 0
 soma = soma + retorno
+
+from exercicio01 import somar
+somar(3 , 5)
+
+from exercicio01 import *
+
+frase = "ola gente"
+texto(frase)
+
+from exercicio01 import *
+duplicados(1,2,2,3,4,4,5,6,3,7,6,8)
+
+from exercicio01 import *
+dupli2(100,102,101,104,103,107,105,106)
+
+from exercicio01 import *
+
+print(primo(31))
