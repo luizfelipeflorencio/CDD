82226941be28f385a98476aca63938a0f2c966c5
Luiz Felipe Florencio Monteiro <165718604+luizfelipeflorencio@users.noreply.github.com>
2024-06-04 13:47:52 -0300

    Delete Aula06 directory

--- Aula06/exercicio01.py
+++
@@ -1,6 +0,0 @@
-nomes=["","","","",""]
-for x in range(5):
-    nomes[x]=input("Digite os nomes: ")
-
-for z in range(5):
-    print(nomes[z])
--- Aula06/exercicio06.py
+++
@@ -1,7 +0,0 @@
-nomes= ["","","","",""]
-senhas= [0,0,0,0,0]
-for x in range(5):
-    nomes[x]= input("Digite o nome: ")
-    senhas[x] = int(input("Digite a senha: "))
-for y in range(5):
-    print(nomes[y],senhas[y],y)
