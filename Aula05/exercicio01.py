a81dcd5e18dbbf445ad886b66d5eacd6549d8621
Luiz Felipe Florencio Monteiro <165718604+luizfelipeflorencio@users.noreply.github.com>
2024-06-04 13:47:43 -0300

    Delete Aula05 directory

--- Aula05/exercicio01.py
+++
@@ -1,7 +0,0 @@
-v1=int(input("digite o primeiro valor: "))
-v2=int(input("digite o segundo valor: "))
-x = 0
-while v2==0:
-        v2 = int(input("Digite o segundo valor novamente: "))
-divisao =  v1/v2
-print(divisao)
